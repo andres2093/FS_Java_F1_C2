f9e20d2c46986a7c48bf2a7645c7d927826422d1
Andrés Ramírez <andres200493@gmail.com>
2023-09-11 19:30:26 -0600

    S4 -> base

---
+++ s4/src/main/java/org/bedu/java/backend/s4/S4Application.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.s4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S4Application.class, args);
+	}
+
+}
---
+++ s4/src/test/java/org/bedu/java/backend/s4/S4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.s4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
