0b7246d126c5fb2346c8d7e2f3473c6991ce9ed2
Andrés Ramírez <andres200493@gmail.com>
2023-09-11 21:13:36 -0600

    S4 -> R1

--- s4/src/main/java/org/bedu/java/backend/s4/controllers/SaludoController.java
+++ s4/src/main/java/org/bedu/java/backend/s4/controllers/SaludoController.java
@@ -2,6 +2,7 @@ package org.bedu.java.backend.s4.controllers;
 
 import org.bedu.java.backend.s4.models.Saludo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,16 @@ public class SaludoController {
     public Saludo saluda(){
         Saludo saludo = new Saludo();
         saludo.setMensaje("Hola Bedu!!!");
+        return saludo;
+    }
+
+    @GetMapping("/saludo/{nombre}")
+    public Saludo saluda(@PathVariable String nombre){
+
+        Saludo saludo = new Saludo();
+        saludo.setMensaje("Hola Bedu!!!");
+        saludo.setNombre(nombre);
+
         return saludo;
     }
 }
