9e273773313487c391725a2fdb0a8f525be8d5ff
Andrés Ramírez <andres200493@gmail.com>
2023-09-13 19:23:49 -0600

    S5 -> base

---
+++ s5/src/main/java/org/bedu/java/backend/s5/S5Application.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.s5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5Application.class, args);
+	}
+
+}
