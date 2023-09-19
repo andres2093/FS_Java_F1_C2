f3257642ba24ced9f19bcc75be80ab594477a5b5
Andrés Ramírez <andres200493@gmail.com>
2023-09-18 20:40:36 -0600

    S6 -> R1

--- s6/src/main/java/org/bedu/java/backend/s6/e1/controllers/UsuarioController.java
+++ s6/src/main/java/org/bedu/java/backend/s6/e1/controllers/UsuarioController.java
@@ -30,4 +30,17 @@ public class UsuarioController {
 
         return "Usuario " + id + " creado!!!";
     }
+
+    @GetMapping
+    public String creaUsuarioR1(@RequestBody Usuario usuario, @RequestParam("id") long id, @RequestParam("rol") String rol){
+        System.out.println("Creando usuario");
+        System.out.println("Nombre: " + usuario.getNombre());
+        System.out.println("Apellido: " + usuario.getApellido());
+        System.out.println("Usuario: " + usuario.getUsuario());
+        System.out.println("Correo: " + usuario.getCorreoElectronico());
+        System.out.println("Contraseña: " + usuario.getContrasenia());
+        System.out.println("Rol: " + rol);
+
+        return "Usuario " + id + " creado!!!";
+    }
 }
