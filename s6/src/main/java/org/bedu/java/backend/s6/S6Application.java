496123f942d09ad1fcf9a4edbf9da4be8dc89d73
Andrés Ramírez <andres200493@gmail.com>
2023-09-18 19:20:37 -0600

    S6 -> base

---
+++ s6/src/main/java/org/bedu/java/backend/s6/S6Application.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.s6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6Application.class, args);
+	}
+
+}
---
+++ s6/src/test/java/org/bedu/java/backend/s6/S6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.s6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
