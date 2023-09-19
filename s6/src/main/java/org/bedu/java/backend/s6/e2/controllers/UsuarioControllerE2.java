eb9093661f93005d015c2ac2792c6233620aedb4
Andrés Ramírez <andres200493@gmail.com>
2023-09-18 21:25:27 -0600

    S6 -> E2.2

--- s6/src/main/java/org/bedu/java/backend/s6/e2/controllers/UsuarioControllerE2.java
+++ s6/src/main/java/org/bedu/java/backend/s6/e2/controllers/UsuarioControllerE2.java
@@ -2,6 +2,8 @@ package org.bedu.java.backend.s6.e2.controllers;
 
 import org.bedu.java.backend.s6.e2.models.Direccion;
 import org.bedu.java.backend.s6.e2.models.UsuarioE2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioControllerE2 {
 
     @PostMapping
-    public String creaUsuario(@RequestBody UsuarioE2 usuario){
+    public ResponseEntity<UsuarioE2> creaUsuario(@RequestBody UsuarioE2 usuario){
         System.out.println("Creando usuario");
         System.out.println("Nombre: " + usuario.getNombre());
         System.out.println("Apellido: " + usuario.getApellido());
@@ -27,6 +29,7 @@ public class UsuarioControllerE2 {
 
         usuario.setDireccion(direccion);
 
-        return "Usuario creado!!!";
+//        return ResponseEntity.ok(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).eTag("Prueba").body(usuario);
     }
 }
