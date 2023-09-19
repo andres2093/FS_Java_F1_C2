a3a218e53671282372862654f37446201b0758c6
Andrés Ramírez <andres200493@gmail.com>
2023-09-18 19:52:52 -0600

    S6 -> E1.2

--- s6/src/main/java/org/bedu/java/backend/s6/e1/controllers/UsuarioController.java
+++ s6/src/main/java/org/bedu/java/backend/s6/e1/controllers/UsuarioController.java
@@ -1,10 +1,7 @@
 package org.bedu.java.backend.s6.e1.controllers;
 
 import org.bedu.java.backend.s6.e1.models.Usuario;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/usuario")
@@ -20,5 +17,17 @@ public class UsuarioController {
         System.out.println("Contraseña: " + usuario.getContrasenia());
 
         return "Usuario creado!!!";
+    }
+
+    @PostMapping("/{id}")
+    public String creaUsuario(@RequestBody Usuario usuario, @PathVariable("id") long id){
+        System.out.println("Creando usuario");
+        System.out.println("Nombre: " + usuario.getNombre());
+        System.out.println("Apellido: " + usuario.getApellido());
+        System.out.println("Usuario: " + usuario.getUsuario());
+        System.out.println("Correo: " + usuario.getCorreoElectronico());
+        System.out.println("Contraseña: " + usuario.getContrasenia());
+
+        return "Usuario " + id + " creado!!!";
     }
 }
