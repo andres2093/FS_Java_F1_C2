20cc08d06b5ba194cfe0d59bfbfe6863011a5459
Andrés Ramírez <andres200493@gmail.com>
2023-09-20 21:30:18 -0600

    S7 -> E3

--- s7/src/main/java/org/bedu/java/backend/s7/controllers/UsuarioController.java
+++ s7/src/main/java/org/bedu/java/backend/s7/controllers/UsuarioController.java
@@ -1,8 +1,10 @@
 package org.bedu.java.backend.s7.controllers;
 
+import jakarta.validation.Valid;
 import org.bedu.java.backend.s7.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,8 +19,14 @@ public class UsuarioController {
     }
 
     @PostMapping("/registro")
-    public ModelAndView registro(Usuario usuario){
-        ModelAndView modelAndView = new ModelAndView("registroExitoso");
+    public ModelAndView registro(@Valid Usuario usuario, Errors errors){
+        String vista = "registroExitoso";
+
+        if (errors.hasErrors()) {
+            vista = "index";
+        }
+
+        ModelAndView modelAndView = new ModelAndView(vista);
         modelAndView.addObject("usuario", usuario);
         return modelAndView;
     }
--- s7/src/main/java/org/bedu/java/backend/s7/models/Usuario.java
+++ s7/src/main/java/org/bedu/java/backend/s7/models/Usuario.java
@@ -1,9 +1,26 @@
 package org.bedu.java.backend.s7.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
 public class Usuario {
+    @NotBlank(message = " Nombre es obligatorio")
     private String nombre;
+    @NotBlank(message = " Correo es obligatorio")
+    @Email(message = " Escribe un correo valido")
+    private String correo;
+    @NotBlank(message = " Usuario es obligatorio")
+    @Length(min = 8, max = 20, message = " Usuario debe tener entre 8 y 20 caracteres")
     private String usuario;
+    @NotBlank(message = " Contraseña es obligatorio")
     private String contrasenia;
+    @NotBlank(message = " Teléfono es obligatorio")
+    @Pattern(regexp = "^(\\d{2,4}[- .]?){2}\\d{4}$", message = " El teléfono debe tener formato ##-####-####")
+    private String telefono;
+    @NotBlank(message = " Rol es obligatorio")
     private String rol;
 
     public String getNombre() {
@@ -12,6 +29,14 @@ public class Usuario {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 
     public String getUsuario() {
@@ -28,6 +53,14 @@ public class Usuario {
 
     public void setContrasenia(String contrasenia) {
         this.contrasenia = contrasenia;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
     }
 
     public String getRol() {
