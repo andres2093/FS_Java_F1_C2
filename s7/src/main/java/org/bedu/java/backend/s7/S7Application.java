76f2f00eb3a733022f3343091cdf98c70a4ceb41
Andrés Ramírez <andres200493@gmail.com>
2023-09-20 19:25:16 -0600

    S7 -> base

---
+++ s7/src/main/java/org/bedu/java/backend/s7/S7Application.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.s7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7Application.class, args);
+	}
+
+}
---
+++ s7/src/test/java/org/bedu/java/backend/s7/S7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.s7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
