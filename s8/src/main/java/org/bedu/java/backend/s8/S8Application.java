aaca9b2ecd77dbcdf57060f21e06706b1f6d2546
Andrés Ramírez <andres200493@gmail.com>
2023-09-25 19:32:28 -0600

    S8 -> base

---
+++ s8/src/main/java/org/bedu/java/backend/s8/S8Application.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.s8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S8Application.class, args);
+	}
+
+}
---
+++ s8/src/test/java/org/bedu/java/backend/s8/S8ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.s8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
